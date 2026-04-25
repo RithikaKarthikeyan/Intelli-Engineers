fb0d78f49aaa02fdefe84659d48704752d0aceea
RithikaKarthikeyan <rithikakarthik22@gmail.com>
2026-04-25 20:12:28 +0530

    initial commit

---
+++ frontend/main.js
@@ -0,0 +1,25 @@
+
+
+function navigate(page) {
+
+alert("Opening " + page);
+
+/* Later change to:
+
+window.location.href = page + ".html";
+
+*/
+
+}
+
+function applySuggestion() {
+
+alert("Suggestion Applied!");
+
+}
+
+function signOut() {
+
+alert("Signed Out");
+
+}
